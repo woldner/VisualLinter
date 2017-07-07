95ba0401715417eed1a7388991ab061fa97b995f
Jean Alexander Woldner <alexander@woldner.io>
2017-07-07 18:12:04 +0200

    add RegexHelper

---
+++ VisualLinter/Helpers/RegexHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace jwldnr.VisualLinter.Helpers
+{
+    internal static class RegexHelper
+    {
+        private const string Pattern = "^[\t ]*$|[^\\s\\/\\\\\\(\\)\"\':,\\.;<>~!@#\\$%\\^&\\*\\|\\+=\\[\\]\\{\\}`\\?\\-…]+";
+
+        internal static Match GetWord(string value) => Regex.Match(value, Pattern);
+    }
+}
