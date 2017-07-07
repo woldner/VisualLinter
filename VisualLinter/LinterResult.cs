9f4aa23f0b2f811fbd8ae04489258c189ebddcdb
Jean Alexander Woldner <alexander@woldner.io>
2017-07-07 20:22:07 +0200

    add LinterResult

---
+++ VisualLinter/LinterResult.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace jwldnr.VisualLinter
+{
+    [JsonObject]
+    internal class LinterResult
+    {
+        [JsonProperty("errorCount")]
+        public int ErrorCount { get; set; }
+
+        [JsonProperty("filePath")]
+        public string FilePath { get; set; }
+
+        [JsonProperty("messages")]
+        public IEnumerable<LinterMessage> Messages { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("warningCount")]
+        public int WarningCount { get; set; }
+    }
+}
