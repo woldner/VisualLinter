7b09cb379ddded8a808d72161364d0388053974f
Jean Alexander Woldner <alexander@woldner.io>
2017-07-07 20:42:38 +0200

    add MessageMarker

---
+++ VisualLinter/MessageMarker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.Text;
+
+namespace jwldnr.VisualLinter
+{
+    internal class MessageMarker
+    {
+        public LinterMessage Message { get; }
+
+        public SnapshotSpan Span { get; }
+
+        internal MessageMarker(LinterMessage message, SnapshotSpan span)
+        {
+            Message = message;
+            Span = span;
+        }
+
+        public MessageMarker CloneAndTranslateTo(ITextSnapshot newSnapshot)
+        {
+            var newSpan = Span.TranslateTo(newSnapshot, SpanTrackingMode.EdgeExclusive);
+
+            return newSpan.Length == Span.Length
+                ? new MessageMarker(Message, newSpan)
+                : null;
+        }
+    }
+}
