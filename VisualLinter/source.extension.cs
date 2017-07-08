ed7157aae8a0f0c438b836a0fce88c7696ac90c5
Jean Alexander Woldner <alexander@woldner.io>
2017-07-08 17:55:23 +0200

    Revert "update vsix guid"
    This reverts commit 76ad2f72a6b7dd09b7d8e6135f3a3ede556bb0ab.

--- VisualLinter/source.extension.cs
+++ VisualLinter/source.extension.cs
@@ -7,11 +7,11 @@ namespace jwldnr.VisualLinter
 {
     static class Vsix
     {
-        public const string Id = "21d9f99b-ec42-4df4-8b16-2a62db5392a5";
+        public const string Id = "832aee43-88e1-4e51-ac31-d412d356dfdf";
         public const string Name = "VisualLinter";
         public const string Description = @"Visual Studio Linter using ESLint (installed globally) with visual feedback in the text buffer.";
         public const string Language = "en-US";
-        public const string Version = "1.0.27";
+        public const string Version = "1.0.26";
         public const string Author = "Jean Alexander Woldner";
         public const string Tags = "Visual Studio, visual studio 2017, lint, linter, Linting, ESLint, Javascript";
     }
