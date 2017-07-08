cf00bcacaec18552a95022d9e112cd6801730561
Jean Alexander Woldner <alexander@woldner.io>
2017-07-08 10:47:52 +0200

    cleanup SinkManager

--- VisualLinter/SinkManager.cs
+++ VisualLinter/SinkManager.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Microsoft.VisualStudio.Shell.TableManager;
+﻿using Microsoft.VisualStudio.Shell.TableManager;
+using System;
 
 namespace jwldnr.VisualLinter
 {
     internal class SinkManager : IDisposable
     {
         private readonly TaggerProvider _provider;
-
         private readonly ITableDataSink _sink;
 
         internal SinkManager(TaggerProvider provider, ITableDataSink sink)
@@ -17,9 +16,9 @@ namespace jwldnr.VisualLinter
             _provider.AddSinkManager(this);
         }
 
-        internal void AddFactory(SnapshotFactory factory) => _sink.AddFactory(factory);
-
         public void Dispose() => _provider.RemoveSinkManager(this);
+
+        internal void AddFactory(SnapshotFactory factory) => _sink.AddFactory(factory);
 
         internal void RemoveFactory(SnapshotFactory factory) => _sink.RemoveFactory(factory);
 
