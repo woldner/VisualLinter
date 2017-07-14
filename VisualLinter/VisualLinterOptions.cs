7871d29e5d3075aff2c8f79d4455ebf23148ebf0
Jean Alexander Woldner <alexander@woldner.io>
2017-07-14 20:45:39 +0200

    add options page

---
+++ VisualLinter/VisualLinterOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Shell;
+using System.ComponentModel;
+
+namespace jwldnr.VisualLinter
+{
+    internal class VisualLinterOptions : DialogPage
+    {
+        [Category("General")]
+        [DisplayName("Use Environment Variables to locate ESLint")]
+        [DefaultValue(true)]
+        public bool UseEnvironmentVariables => Settings.UseEnvironmentVariables;
+
+        private IVisualLinterSettings Settings => _settings ?? (_settings = GetSettings());
+        private IVisualLinterSettings _settings;
+
+        private static IVisualLinterSettings GetSettings()
+        {
+            var componentModel = ServiceProvider.GlobalProvider.GetService(typeof(SComponentModel)) as IComponentModel;
+            return componentModel?.DefaultExportProvider.GetExportedValue<IVisualLinterSettings>();
+        }
+    }
+}
--- VisualLinter/VisualLinterSettings.cs
+++ VisualLinter/VisualLinterSettings.cs
@@ -1,34 +1,33 @@
-﻿using System;
-using System.ComponentModel.Composition;
-using Microsoft.VisualStudio.Settings;
+﻿using Microsoft.VisualStudio.Settings;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Settings;
+using System;
+using System.ComponentModel.Composition;
 
 namespace jwldnr.VisualLinter
 {
     internal interface IVisualLinterSettings
     {
-        bool Value { get; }
+        bool UseEnvironmentVariables { get; }
     }
 
     [Export(typeof(IVisualLinterSettings))]
     [PartCreationPolicy(CreationPolicy.Shared)]
     internal class VisualLinterSettings : IVisualLinterSettings
     {
-        public bool Value => true;
+        public bool UseEnvironmentVariables => true;
 
         [ImportingConstructor]
         internal VisualLinterSettings(SVsServiceProvider serviceProvider)
             : this(new ShellSettingsManager(serviceProvider))
         {
+            // provider has to be of type SVsServiceProvider
         }
 
         internal VisualLinterSettings(SettingsManager settingsManager)
         {
             if (null == settingsManager)
                 throw new ArgumentNullException(nameof(settingsManager));
-
-
         }
     }
 }
