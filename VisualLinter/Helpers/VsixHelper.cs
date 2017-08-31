4185eee0a54187e5dbb7887ca54d8fcb42d060cf
Jean Alexander Woldner <alexander@woldner.io>
2017-08-31 20:30:30 +0200

    move logic from vsixhelper to linterhelper

--- VisualLinter/Helpers/VsixHelper.cs
+++ VisualLinter/Helpers/VsixHelper.cs
@@ -2,69 +2,20 @@
 using Microsoft.VisualStudio.Shell;
 using System;
 using System.IO;
-using System.Linq;
 
 namespace jwldnr.VisualLinter.Helpers
 {
     internal static class VsixHelper
     {
-        private static readonly string[] ValidConfigNames = {
-            ".eslintrc.js",
-            ".eslintrc.yaml",
-            ".eslintrc.yml",
-            ".eslintrc.json",
-            ".eslintrc"
-        };
-
-        internal static string GetGlobalConfigPath()
-        {
-            var userDirectoryPath = GetUserDirectoryPath();
-
-            var foundConfigs = Directory.EnumerateFiles(userDirectoryPath, ".eslintrc*", SearchOption.TopDirectoryOnly)
-                .Where(filePath => -1 != Array.IndexOf(ValidConfigNames, Path.GetFileName(filePath)))
-                .OrderBy(filePath => Array.IndexOf(ValidConfigNames, Path.GetFileName(filePath)));
-
-            return foundConfigs.Any()
-                ? foundConfigs.First()
-                : null;
-        }
-
-        internal static string GetLocalConfigPath(string filePath)
-        {
-            // never traverse directory further than the solution root
-            var solutionFolder = new DirectoryInfo(GetSolutionPath());
-
-            var filePathDirectory = Path.GetDirectoryName(filePath)
-                ?? throw new Exception("could not get working directory name");
-
-            var currentDirectory = new DirectoryInfo(filePathDirectory);
-
-            // todo: instead of comparing length.. ugh
-            while (null != currentDirectory && currentDirectory.FullName.Length >= solutionFolder.FullName.Length)
-            {
-                var foundConfigs = currentDirectory.EnumerateFiles(".eslintrc*")
-                    .Where(fileInfo => -1 != Array.IndexOf(ValidConfigNames, fileInfo.Name))
-                    .OrderBy(fileInfo => Array.IndexOf(ValidConfigNames, fileInfo.Name));
-
-                if (foundConfigs.Any())
-                    return foundConfigs.First().FullName;
-
-                currentDirectory = currentDirectory.Parent;
-            }
-
-            return null;
-        }
-
         internal static string GetProjectName(string filePath)
         {
             try
             {
                 return GetProject(filePath).Name;
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                OutputWindowHelper.WriteLine($"unable to get project name for file: {Path.GetFileName(filePath)}"
-                    + $"\nerror message: {e.Message}");
+                OutputWindowHelper.WriteLine($"warning: unable to get project name for file: {filePath}.");
             }
 
             return null;
@@ -80,21 +31,11 @@ namespace jwldnr.VisualLinter.Helpers
 
         private static Project GetProject(string filePath)
         {
-            try
-            {
-                var solution = GetSolution();
+            var solution = GetSolution();
 
-                var item = solution?.FindProjectItem(filePath);
+            var item = solution?.FindProjectItem(filePath);
 
-                return item?.ContainingProject;
-            }
-            catch (Exception e)
-            {
-                OutputWindowHelper.WriteLine($"unable to get project for file: {Path.GetFileName(filePath)}"
-                    + $"\nerror message: {e.Message}");
-            }
-
-            return null;
+            return item?.ContainingProject;
         }
 
         private static Solution GetSolution()
@@ -102,11 +43,6 @@ namespace jwldnr.VisualLinter.Helpers
             var dte = Package.GetGlobalService(typeof(DTE)) as DTE;
 
             return dte?.Solution;
-        }
-
-        private static string GetUserDirectoryPath()
-        {
-            return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         }
     }
 }
