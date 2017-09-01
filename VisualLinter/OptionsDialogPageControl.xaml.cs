e01b7841f53698956ef242a962c7738026cc65ea
Jean Alexander Woldner <alexander@woldner.io>
2017-09-01 07:43:21 +0200

    add new options control

---
+++ VisualLinter/OptionsDialogPageControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using jwldnr.VisualLinter.Helpers;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace jwldnr.VisualLinter
+{
+    /// <summary>
+    /// Interaction logic for OptionsDialogPageControl.xaml
+    /// </summary>
+    public partial class OptionsDialogPageControl
+    {
+        internal bool UseGlobalConfig
+        {
+            get => UsePersonalConfigCheckBox.IsChecked ?? false;
+            set => UsePersonalConfigCheckBox.IsChecked = value;
+        }
+
+        internal bool UseGlobalLinter
+        {
+            get => UseGlobalLinterCheckBox.IsChecked ?? false;
+            set => UseGlobalLinterCheckBox.IsChecked = value;
+        }
+
+        internal OptionsDialogPageControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SuggestNewFeatures_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+        }
+
+        private void UseGlobalLinter_OnClick(object sender, RoutedEventArgs e)
+        {
+            var value = UseGlobalLinter.ToString().ToLowerInvariant();
+            OutputWindowHelper.WriteLine($"always use global linter option set to: {value}");
+        }
+
+        private void UsePersonalConfig_OnClick(object sender, RoutedEventArgs e)
+        {
+            var value = UseGlobalConfig.ToString().ToLowerInvariant();
+            OutputWindowHelper.WriteLine($"use personal config option set to: {value}");
+        }
+    }
+}
