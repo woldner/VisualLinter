ba9f3093f013cc8231743fb93e4ccef9363d7143
Jean Alexander Woldner <alexander@woldner.io>
2017-09-11 07:57:46 +0200

    WaitForExitAsync

--- VisualLinter/Linting/Linter.cs
+++ VisualLinter/Linting/Linter.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
 
 namespace jwldnr.VisualLinter.Linting
 {
@@ -73,7 +74,7 @@ namespace jwldnr.VisualLinter.Linting
                 if (false == string.IsNullOrEmpty(error))
                     OutputWindowHelper.WriteLine(error);
 
-                process.WaitForExit();
+                await process.WaitForExitAsync();
 
                 try
                 {
