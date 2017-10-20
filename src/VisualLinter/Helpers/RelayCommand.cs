681e7a97d96359c5dc819ab5ae2885a4cacd37a0
Jean Alexander Woldner <alexander@woldner.io>
2017-10-20 19:21:26 +0200

    generic relaycommand

--- src/VisualLinter/Helpers/RelayCommand.cs
+++ src/VisualLinter/Helpers/RelayCommand.cs
@@ -3,16 +3,16 @@ using System.Windows.Input;
 
 namespace jwldnr.VisualLinter.Helpers
 {
-    internal class RelayCommand : ICommand
+    internal class RelayCommand<T> : ICommand
     {
-        private readonly Func<bool> _canExecute;
-        private readonly Action _execute;
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
 
-        internal RelayCommand(Action execute) : this(execute, null)
+        internal RelayCommand(Action<T> execute) : this(execute, null)
         {
         }
 
-        internal RelayCommand(Action execute, Func<bool> canExecute)
+        internal RelayCommand(Action<T> execute, Func<T, bool> canExecute)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
@@ -22,10 +22,10 @@ namespace jwldnr.VisualLinter.Helpers
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null || _canExecute();
+            return _canExecute == null || _canExecute((T)parameter);
         }
 
-        public void Execute(object parameter) => _execute();
+        public void Execute(object parameter) => _execute((T)parameter);
 
         internal void RaiseCanExecuteChanged()
         {
