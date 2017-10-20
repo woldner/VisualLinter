0d33940ae320b8bd9ff487ef93dc853916a53d65
Jean Alexander Woldner <alexander@woldner.io>
2017-10-20 18:11:34 +0200

    add optionsdialogviewmodel | add references

---
+++ src/VisualLinter/ViewModels/OptionsDialogViewModel.cs
@@ -0,0 +1,84 @@
+﻿using jwldnr.VisualLinter.Helpers;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Windows;
+
+namespace jwldnr.VisualLinter.ViewModels
+{
+    internal class OptionsDialogViewModel : ViewModelBase
+    {
+        internal bool DisableIgnorePath
+        {
+            get => GetPropertyValue<bool>(DisableIgnorePathProperty);
+            set => SetPropertyValue(DisableIgnorePathProperty, value);
+        }
+
+        internal bool UseGlobalEslint
+        {
+            get => GetPropertyValue<bool>(UseGlobalEslintProperty);
+            set => SetPropertyValue(UseGlobalEslintProperty, value);
+        }
+
+        internal bool UsePersonalConfig
+        {
+            get => GetPropertyValue<bool>(UsePersonalConfigProperty);
+            set => SetPropertyValue(UsePersonalConfigProperty, value);
+        }
+
+        internal static readonly DependencyProperty DisableIgnorePathProperty =
+            DependencyProperty.Register(
+                nameof(DisableIgnorePath),
+                typeof(bool),
+                typeof(OptionsDialogViewModel),
+                new PropertyMetadata(default(bool)));
+
+        internal static readonly DependencyProperty UseGlobalEslintProperty =
+            DependencyProperty.Register(
+                nameof(UseGlobalEslint),
+                typeof(bool),
+                typeof(OptionsDialogViewModel),
+                new PropertyMetadata(default(bool)));
+
+        internal static readonly DependencyProperty UsePersonalConfigProperty =
+            DependencyProperty.Register(
+                nameof(UsePersonalConfig),
+                typeof(bool),
+                typeof(OptionsDialogViewModel),
+                new PropertyMetadata(default(bool)));
+
+        private readonly IVisualLinterOptions _options;
+
+        internal OptionsDialogViewModel()
+        {
+            _options = ServiceProvider.GlobalProvider.GetMefService<IVisualLinterOptions>()
+                ?? throw new Exception("fatal: unable to retrieve options");
+        }
+
+        internal void Apply()
+        {
+            _options.UseGlobalEslint = UseGlobalEslint;
+            _options.UsePersonalConfig = UsePersonalConfig;
+            _options.DisableIgnorePath = DisableIgnorePath;
+        }
+
+        internal void Initiailize()
+        {
+            LoadOptions();
+            RaiseAllChanged();
+        }
+
+        private void LoadOptions()
+        {
+            UseGlobalEslint = _options.UseGlobalEslint;
+            UsePersonalConfig = _options.UsePersonalConfig;
+            DisableIgnorePath = _options.DisableIgnorePath;
+        }
+
+        private void RaiseAllChanged()
+        {
+            RaisePropertyChanged(nameof(UseGlobalEslint));
+            RaisePropertyChanged(nameof(UsePersonalConfig));
+            RaisePropertyChanged(nameof(DisableIgnorePath));
+        }
+    }
+}
