2112e5afc5244b8bb99543c7611f4967a2e74313
Jean Alexander Woldner <alexander@woldner.io>
2018-12-29 02:57:05 -0800

    add GetService provider extension method

--- src/VisualLinter/Helpers/ServiceProviderExtensions.cs
+++ src/VisualLinter/Helpers/ServiceProviderExtensions.cs
@@ -5,11 +5,24 @@ namespace jwldnr.VisualLinter.Helpers
 {
     internal static class ServiceProviderExtensions
     {
-        internal static T GetMefService<T>(this IServiceProvider serviceProvider) where T : class
+        internal static T GetMefService<T>(this IServiceProvider serviceProvider)
+            where T : class
         {
             var componentModel = serviceProvider.GetService(typeof(SComponentModel)) as IComponentModel;
 
             return componentModel?.GetService<T>();
+        }
+
+        internal static TU GetService<T, TU>(this IServiceProvider serviceProvider)
+            where T : class
+            where TU : class
+        {
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            return serviceProvider.GetService(typeof(T)) as TU;
         }
     }
 }
